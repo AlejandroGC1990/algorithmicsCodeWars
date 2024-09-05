3df678ca1dce170b0ca975dca139f0c9ef07ba10
Alex E. C. <alejandro.garcia.carmona@gmail.com>
2024-09-05 12:56:23 +0200

    kata done

---
+++ 8kyü/Is_it_a_palindrome.js
@@ -0,0 +1,17 @@
+//? Write a function that checks if a given string 
+//? (case insensitive) is a palindrome.
+//? A palindrome is a word, number, phrase, or other 
+//? sequence of symbols that reads the same backwards 
+//? as forwards, such as madam or racecar.
+
+//? Escriba una función que verifique si una cadena 
+//? dada (sin distinguir entre mayúsculas y minúsculas)
+//? es un palíndromo .
+//? Un palíndromo es una palabra, número, frase u otra 
+//? secuencia de símbolos que se lee igual al revés que
+//? hacia adelante, como por ejemplo madamo racecar.
+
+function isPalindrome(x) {
+    const cleanedString = x.toLowerCase().replace(/[^a-z0-9]/g, '');
+    return cleanedString === cleanedString.split('').reverse().join('');
+  }
