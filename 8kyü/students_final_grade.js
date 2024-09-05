cd5010decdff5ab512b4b072db29680bad3f0227
Alex E. C. <alejandro.garcia.carmona@gmail.com>
2024-09-05 13:15:40 +0200

    kata done

---
+++ 8kyü/students_final_grade.js
@@ -0,0 +1,58 @@
+//? Create a function finalGrade, which calculates the final grade
+//? of a student depending on two parameters: a grade for the exam
+//? and a number of completed projects.
+//? This function should take two arguments: exam - grade for exam
+//? (from 0 to 100); projects - number of completed projects
+//? (from 0 and above);
+//? This function should return a number (final grade). There are
+//? four types of final grades:
+//?  -100, if a grade for the exam is more than 90 or if a number of completed projects more than 10.
+//?  -90, if a grade for the exam is more than 75 and if a number of completed projects is minimum 5.
+//?  -75, if a grade for the exam is more than 50 and if a number of completed projects is minimum 2.
+//?  -0, in other cases
+
+//? Crea una función finalGrade, que calcula la nota final de un
+//? alumno en función de dos parámetros: una nota del examen y un
+//? número de proyectos completados.
+//? Esta función debe tomar dos argumentos: examen: calificación
+//? del examen (de 0 a 100); proyectos: número de proyectos
+//? completados (de 0 en adelante);
+//? Esta función debe devolver un número (calificación final).
+//? Existen cuatro tipos de calificaciones finales:
+//?  -100, si la nota del examen es superior a 90 o si el número de proyectos completados es superior a 10.
+//?  -90, si la nota del examen es superior a 75 y si el número de proyectos completados es mínimo 5.
+//?  -75, si la nota del examen es superior a 50 y si el número de proyectos realizados es mínimo 2.
+//?  -0, en otros casos
+
+// Ejemplos ( Entradas --> Salida ):
+
+// 100, 12 --> 100
+// 99, 0 --> 100
+// 10, 15 --> 100
+
+// 85, 5 --> 90
+
+// 55, 3 --> 75
+
+// 55, 0 --> 0
+// 20, 2 --> 0
+
+function finalGrade(exam, projects) {
+  if (exam > 90 || projects > 10) {
+    return 100;
+  } else if (exam > 75 && projects >= 5) {
+    return 90;
+  } else if (exam > 50 && projects >= 2) {
+    return 75;
+  } else {
+    return 0;
+  }
+}
+
+console.log(finalGrade(100, 12)); // 100
+console.log(finalGrade(99, 0)); // 100
+console.log(finalGrade(10, 15)); // 100
+console.log(finalGrade(85, 5)); // 90
+console.log(finalGrade(55, 3)); // 75
+console.log(finalGrade(55, 0)); // 0
+console.log(finalGrade(20, 2)); // 0
