fe247e0a0e8a25a332a872928beaf2179cc51eb6
Alex E. C. <alejandro.garcia.carmona@gmail.com>
2024-09-05 13:26:30 +0200

    kata done

---
+++ 8kyü/convert_a_boolean_to_a_string.js
@@ -0,0 +1,14 @@
+//? Implement a function which convert the given boolean value
+//? into its string representation.
+//? Note: Only valid inputs will be given.
+
+//? Implementa una función que convierta el valor booleano dado en
+//? su representación de cadena.
+//? Nota: Sólo se proporcionarán entradas válidas.
+
+function booleanToString(b) {
+  return b.toString();
+}
+
+console.log(booleanToString(true)); // "true"
+console.log(booleanToString(false)); // "false"
