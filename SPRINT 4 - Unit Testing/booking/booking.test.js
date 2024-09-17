c5ceab48f1a58c1f74b40db840b4b9f142ecf6ed
Alex E. C. <alejandro.garcia.carmona@gmail.com>
2024-09-17 18:14:25 +0200

    add booking.test.js

--- SPRINT 4 - Unit Testing/booking/booking.test.js
+++ SPRINT 4 - Unit Testing/booking/booking.test.js
@@ -1,1 +1,11 @@
+const Booking = require("./booking");
+const Room = require("../room/room"); 
 
+describe("Booking Class Tests", () => {
+  test("Booking should calculate the fee correctly", () => {
+    const room = new Room("Room 1", [], 10000, 10); 
+    const booking = new Booking("John Doe", "john@example.com", new Date("2024-09-15"), new Date("2024-09-20"), 5, room); 
+    
+    expect(booking.fee).toBe(45000);
+  });
+});
