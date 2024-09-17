cc62de0f3267b642527f09b290f0ff66e5189f8b
Alex E. C. <alejandro.garcia.carmona@gmail.com>
2024-09-17 20:07:35 +0200

    add test totalOccupancyPercentage

--- SPRINT 4 - Unit Testing/room/room.test.js
+++ SPRINT 4 - Unit Testing/room/room.test.js
@@ -61,4 +61,62 @@ describe("Room Class Tests", () => {
     expect(occupancy).toBeCloseTo(expectedOccupancy, 2);
   });
 
+  //?totalOccupancyPercentage
+  test("totalOccupancyPercentage should return the correct overall occupancy percentage", () => {
+    const booking1 = new Booking(
+      "John Doe",
+      "john@example.com",
+      new Date("2024-09-01"),
+      new Date("2024-09-10")
+    );
+    const booking2 = new Booking(
+      "Jane Smith",
+      "jane@example.com",
+      new Date("2024-09-05"),
+      new Date("2024-09-15")
+    );
+    const booking3 = new Booking(
+      "Alice Johnson",
+      "alice@example.com",
+      new Date("2024-09-10"),
+      new Date("2024-09-20")
+    );
+
+    const room1 = new Room("Room 1", [booking1], 10000, 10);
+    const room2 = new Room("Room 2", [booking2], 10000, 10);
+    const room3 = new Room("Room 3", [booking3], 10000, 10);
+
+    const startDate = new Date("2024-09-01");
+    const endDate = new Date("2024-09-15");
+
+    const rooms = [room1, room2, room3];
+    const totalOccupancy = Room.totalOccupancyPercentage(
+      rooms,
+      startDate,
+      endDate
+    );
+
+    const totalDays =
+      Math.ceil((endDate - startDate) / (1000 * 60 * 60 * 24)) + 1;
+
+    const occupiedDaysRoom1 =
+      Math.ceil((new Date("2024-09-10") - startDate) / (1000 * 60 * 60 * 24)) +
+      1;
+    const occupiedDaysRoom2 =
+      Math.ceil(
+        (new Date("2024-09-15") - new Date("2024-09-05")) /
+          (1000 * 60 * 60 * 24)
+      ) + 1;
+    const occupiedDaysRoom3 =
+      Math.ceil((endDate - new Date("2024-09-10")) / (1000 * 60 * 60 * 24)) + 1;
+
+    const occupancyRoom1 = (occupiedDaysRoom1 / totalDays) * 100;
+    const occupancyRoom2 = (occupiedDaysRoom2 / totalDays) * 100;
+    const occupancyRoom3 = (occupiedDaysRoom3 / totalDays) * 100;
+
+    const expectedOccupancy =
+      (occupancyRoom1 + occupancyRoom2 + occupancyRoom3) / 3;
+
+    expect(totalOccupancy).toBeCloseTo(expectedOccupancy, 2);
+  });
 });
